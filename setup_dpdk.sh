2b8ec8c3fdb8aa62b5b14c776c5585fbd9ef4f97
repu1sion <bearrailgun@gmail.com>
2016-04-23 18:25:46 +0300

    setup_dpdk.sh improved

--- setup_dpdk.sh
+++ setup_dpdk.sh
@@ -12,6 +12,14 @@ sudo ./dpdk_nic_bind.py -b igb_uio 0000:03:00.0
 sudo ./dpdk_nic_bind.py --status
 
 # setup hugepages
-sudo sysctl -w vm.nr_hugepages=500
+sudo sysctl -w vm.nr_hugepages=512
 sudo sysctl vm.nr_hugepages
 cat /proc/meminfo | grep -i huge
+if [ ! -d /mnt/huge ]; then
+	sudo mkdir /mnt/huge
+fi
+cat /proc/mounts | grep -i huge > /dev/null
+if [ ! $? -eq 0 ]; then
+	sudo mount -t hugetlbfs nodev /mnt/huge
+fi
+cat /proc/mounts | grep -i huge

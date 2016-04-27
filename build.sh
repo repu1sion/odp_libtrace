c9957553a76cdceda1d296f3b097031a91e91b2b
repu1sion <bearrailgun@gmail.com>
2016-04-28 00:11:30 +0300

    build.sh improved - added flag for linker, headers, etc

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ export RTE_SDK=/mnt/raw/gdwk/dpdk/dpdk/
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 
 ./bootstrap.sh
-./configure --with-dpdk=yes CFLAGS=-shared-libgcc
-make
+./configure --with-dpdk=yes CFLAGS="-shared-libgcc -I/mnt/raw/gdwk/odp/odp-dpdk/include -I/mnt/raw/gdwk/odp/odp-dpdk/platform/linux-dpdk/include -I/mnt/raw/gdwk/odp/odp-dpdk/platform/linux-dpdk/arch/x86" LIBS="-lodp-dpdk"
+#./configure --with-dpdk=yes CFLAGS="-shared-libgcc -I/mnt/raw/gdwk/odp/odp-dpdk/include"
+make LIBS="-lodp-dpdk"
 sudo make install
 sudo ldconfig

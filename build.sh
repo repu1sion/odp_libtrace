827759b01a663d33c0ec75ec7e162a49cb3c2773
repu1sion <bearrailgun@gmail.com>
2016-04-28 23:24:48 +0300

    changed build.sh a little bit

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 export RTE_SDK=/mnt/raw/gdwk/dpdk/dpdk/
 export RTE_TARGET=x86_64-native-linuxapp-gcc
+export LDFLAGS='-L/usr/local/lib/'
+#export LIBS='-lodp-dpdk'
 
 ./bootstrap.sh
-./configure --with-dpdk=yes CFLAGS="-shared-libgcc -I/mnt/raw/gdwk/odp/odp-dpdk/include -I/mnt/raw/gdwk/odp/odp-dpdk/platform/linux-dpdk/include -I/mnt/raw/gdwk/odp/odp-dpdk/platform/linux-dpdk/arch/x86" LIBS="-lodp-dpdk"
-#./configure --with-dpdk=yes CFLAGS="-shared-libgcc -I/mnt/raw/gdwk/odp/odp-dpdk/include"
-make LIBS="-lodp-dpdk"
+./configure --with-dpdk=yes CFLAGS="-shared-libgcc -I/mnt/raw/gdwk/odp/odp-dpdk/include -I/mnt/raw/gdwk/odp/odp-dpdk/platform/linux-dpdk/include -I/mnt/raw/gdwk/odp/odp-dpdk/platform/linux-dpdk/arch/x86"
+# make LIBS="-lodp-dpdk"
+make
 sudo make install
 sudo ldconfig

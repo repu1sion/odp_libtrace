a3e1213731d85583fab2098935e45607d5e75ff7
repu1sion <bearrailgun@gmail.com>
2017-06-23 23:47:22 +0300

    script for testing kafka multitopic consuming

---
+++ kafka_multitopic_consuming.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export KAFKA_CONSUME_TOPIC1="capt"
+export KAFKA_CONSUME_TOPIC2="capt2"
+printenv
+
+# we need -E to keep env variables from common user
+sudo -E tracesplit kafka:k erf:trace.erf.gz

a0689433440c3b3959949fce8914c058e10f8c64
repu1sion <bearrailgun@gmail.com>
2017-06-30 17:57:36 +0300

    added userid detection in kafka_multitopic_consuming.sh

--- kafka_multitopic_consuming.sh
+++ kafka_multitopic_consuming.sh
@@ -4,5 +4,13 @@ export KAFKA_CONSUME_TOPIC1="capt"
 export KAFKA_CONSUME_TOPIC2="capt2"
 #printenv
 
-# we need -E to keep env variables from common user
-sudo -E tracesplit kafka:k erf:trace.erf.gz
+USERID=`id -u`
+echo "userid is : $USERID"
+if [ ! $USERID -eq 0 ]; then
+	# we need -E to keep env variables from common user
+	echo "not root. running with sudo -E"
+	sudo -E tracesplit kafka:k erf:trace.erf.gz
+else
+	echo "executing as root"
+	tracesplit kafka:k erf:trace.erf.gz
+fi
